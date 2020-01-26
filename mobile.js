ba32bb1cc3cda86533ff032e9a84d208e80843aa
kimberly <99051407@mydavinci.nl>
2020-01-26 19:02:02 +0100

    5

--- mobile.js
+++ mobile.js
@@ -1,15 +1,32 @@
-var knop= docoment.getElementById("knop");
+var knop= document.getElementById("knop");
 knop.setAttribute('onclick','showMenu()');
 
 	function showMenu(){
-		var menu = document.getelElementById('menu');
+		var menu = document.getElementById('menu');
 
 		if(menu.style.height=="0px" || menu.style.height==""){
 			menu.style.height="auto";
 		}
 		
   else{
-  	menu.style.height=="0px";
+  	menu.style.height="0px";
   }
 	
 }
+
+var knop1= document.getElementById("knop1");
+knop1.setAttribute('onclick','showMenu1()');
+
+	function showMenu1(){
+		var menu1 = document.getElementById('menu1');
+
+		if(menu1.style.height=="0px" || menu1.style.height==""){
+			menu1.style.height="auto";
+		}
+		
+  else{
+  	menu1.style.height="0px";
+  }
+	
+}
+
